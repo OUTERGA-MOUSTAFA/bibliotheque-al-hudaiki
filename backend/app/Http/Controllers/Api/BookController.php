40727ad3da6f682c0fc439086301f789e0530233
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-25 14:29:13 +0000

    feat: add Authentification (Sanctum + Fortify + Permissions & rôles) + Seeders (données démo)

--- backend/app/Http/Controllers/Api/BookController.php
+++ backend/app/Http/Controllers/Api/BookController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Book;
 use App\Http\Requests\StoreBookRequest;
 use App\Http\Resources\BookResource;
